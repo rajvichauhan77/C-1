034487700ced3073c5139970f2cfcb171403bc1a
rajvichauhan77 <chauhanrajvi77@gmail.com>
2024-10-10 17:11:11 +0530

    hgf

---
+++ for loop/index.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    for(int i=0; i<=10; i++){
+        cout << i << endl << endl;
+    }
+
+    cout << "10-0" << endl << endl;
+
+    for(int j=10; j>0; j--){
+        cout << j << endl << endl ;
+    }
+
+    cout << "odd number" << endl << endl << endl ;
+
+    for(int a=1; a<10; a++){
+        if(a%2==1){
+            cout << a << " " ;
+        }
+    }
+
+
+}
